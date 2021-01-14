221a62770f1d54aa1df4c80fd8a91bd8e8a41e14
Akio333 <smulye10@gmail.com>
2021-01-14 14:03:06 +0530

    ADDED: User symbol input.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,9 +1,8 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public char board[] = new char[10];
-
-    TicTacToeGame() {
-        createBoard();
-    }
+    public char userSymbol, computerSymbol;
 
     public void createBoard() {
         for (int i = 1; i < 10; i++) {
@@ -11,9 +10,23 @@ public class TicTacToeGame {
         }
     }
 
+    public void getUserInput(String symbol) {
+        if (symbol.equals("X")) {
+            userSymbol = 'X';
+            computerSymbol = 'O';
+        } else if (symbol.equals("O")) {
+            userSymbol = 'O';
+            computerSymbol = 'X';
+        } else {
+            //
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Wlcome to Tic Tac Toe!");
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        Scanner scanner = new Scanner(System.in);
         ticTacToeGame.createBoard();
+        ticTacToeGame.getUserInput(scanner.next());
     }
 }
