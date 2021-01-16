04b0bca3cf4ff6079f1609e15a9db5d438b25be6
Akio333 <smulye10@gmail.com>
2021-01-16 10:18:46 +0530

    ADDED: Player switching and Winner Declaration.

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -6,6 +6,7 @@ public class TicTacToeGame {
     public char userSymbol, computerSymbol;
     public int player = 0;
     public boolean play = true;
+    public static Scanner scanner = new Scanner(System.in);
 
     public void createBoard() {
         for (int i = 1; i < 10; i++) {
@@ -48,6 +49,7 @@ public class TicTacToeGame {
                 board[index] = userSymbol;
             }
             printBoard();
+            checkWin();
             return false;
         } else {
             return true;
@@ -65,19 +67,109 @@ public class TicTacToeGame {
         }
     }
 
+    public void checkWin() {
+        if ((board[1] == userSymbol && board[2] == userSymbol && board[3] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[4] == userSymbol && board[5] == userSymbol && board[6] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[7] == userSymbol && board[8] == userSymbol && board[9] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == userSymbol && board[4] == userSymbol && board[7] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[2] == userSymbol && board[5] == userSymbol && board[8] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[3] == userSymbol && board[6] == userSymbol && board[9] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == userSymbol && board[5] == userSymbol && board[9] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[3] == userSymbol && board[5] == userSymbol && board[7] == userSymbol)) {
+            System.out.println("Player Wins!");
+            System.exit(0);
+        }
+        if ((board[1] == computerSymbol && board[2] == computerSymbol && board[3] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[4] == computerSymbol && board[5] == computerSymbol && board[6] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[7] == computerSymbol && board[8] == computerSymbol && board[9] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[1] == computerSymbol && board[4] == computerSymbol && board[7] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[2] == computerSymbol && board[5] == computerSymbol && board[8] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[3] == computerSymbol && board[6] == computerSymbol && board[9] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[1] == computerSymbol && board[5] == computerSymbol && board[9] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+        if ((board[3] == computerSymbol && board[5] == computerSymbol && board[7] == computerSymbol)) {
+            System.out.println("Player Lost!");
+            System.exit(0);
+        }
+    }
+
+    public void computerPlay() {
+        // TODO: Implementation computer playing logic
+        while (makeMove((int) (Math.random() * 8) + 1, 0)) {
+            System.out.println("");
+        }
+    }
+
+    public void playGame() {
+        int i;
+        for (i = 0; i < 9; i++) {
+            if (player == 0) {
+                computerPlay();
+                player = 1;
+            } else {
+                System.out.println("Select Position (1-9): ");
+                while (makeMove(scanner.nextInt(), player)) {
+                    System.out.println("Try different place.");
+                }
+                player = 0;
+            }
+        }
+        if (i == 9) {
+            System.out.println("Game Draw!");
+            System.exit(0);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Wlcome to Tic Tac Toe!");
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        Scanner scanner = new Scanner(System.in);
         ticTacToeGame.createBoard();
         System.out.println("Enter Toss (H or T):");
         ticTacToeGame.tossForFirstPlay(scanner.next());
         System.out.println("Choose Your Symbol (O or X): ");
         ticTacToeGame.getUserInput(scanner.next());
         ticTacToeGame.printBoard();
-        System.out.println("Enter Move (1-9): ");
-        while (ticTacToeGame.play) {
-            ticTacToeGame.play = ticTacToeGame.makeMove(scanner.nextInt(), 1);
-        }
+        ticTacToeGame.playGame();
     }
 }
